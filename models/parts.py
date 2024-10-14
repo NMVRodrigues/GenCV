3d7cc44ba293b6e8e21f8d1b0f627decbac3f331
Nuno <nuno.mvrodrigues1@gmail.com>
2024-10-14 23:48:27 +0100

    vae model working, adding KL div loss next

--- models/parts.py
+++ models/parts.py
@@ -58,15 +58,36 @@ class Decoder(nn.Module):
     
 
 class VAE(nn.Module):
-    def __init__(self, in_channels=1, latent_sizes=[32,64,128,256,512]):	
+    def __init__(self, input_size = 32, in_channels=1, latent_sizes=[32,64,128,256,512]):	
         super().__init__()
 
         self.encoder = Encoder(in_channels=in_channels, latent_sizes=latent_sizes)
-        self.decoder = Decoder(out_channels=in_channels, latent_sizes=latent_sizes)
+        self.decoder = Decoder(out_channels=in_channels, latent_sizes=latent_sizes[::-1])
+
+        self.btln_size = input_size//(2**(len(latent_sizes)-1))
+
+        self.mu = nn.Linear(latent_sizes[-1]*(self.btln_size*2), 2)
+        self.logvar = nn.Linear(latent_sizes[-1]*(self.btln_size*2), 2)
+
+        self.decoder_fc = nn.Linear(2, latent_sizes[-1]*(self.btln_size*2))
 
 
 
     def forward(self, x):
         x = self.encoder(x)
-        x = self.decoder(x)
+        # flatten
+        h = x.view(x.size(0), -1)
+        # calculate mu and logvar (distribution parameters)
+        mu = self.mu(h)
+        logvar = self.logvar(h)
+        # reparametrization trick
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        z = mu + eps*std
+        # transform back into the original shape
+        z = self.decoder_fc(z)
+        z = z.view(-1, x.shape[1] , self.btln_size, self.btln_size)
+        # decode
+        x = self.decoder(z)
+
         return x
--- run/vae.py
+++ run/vae.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from models.parts import Encoder, Decoder
+from models.parts import Encoder, Decoder, VAE
 
 data = torch.randn(1, 3, 32, 32)
 
@@ -19,3 +19,8 @@ out_d = decoder(out_e)
 
 print(out_e.shape)
 print(out_d.shape)
+
+vae = VAE(input_size=32, in_channels=3)
+out_vae = vae(data)
+
+print(out_vae.shape)
